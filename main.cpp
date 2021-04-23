034f0acee5038f2ae365136d0556ef0f3749793c
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-04-23 15:51:12 +0800

    Add Testing Variables

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include <Eigen/Dense>
 
 int main() {
-    Eigen::VectorXd vector(5, 1);
-    Eigen::MatrixXd matrix(5, 5);
-    Eigen::Vector3d vector3;
-    Eigen::Matrix3d matrix3;
+    Eigen::VectorXd vector = Eigen::VectorXd::Ones(3, 1);
+    Eigen::MatrixXd matrix = Eigen::MatrixXd::Ones(3, 3);
+    Eigen::Vector3d vector3 = Eigen::Vector3d::Ones();
+    Eigen::Matrix3d matrix3 = Eigen::Matrix3d::Ones();
+
+    using MatrixCustom = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor, 4, 4>;
+    MatrixCustom matrix_custom(2, 2);
+    matrix_custom << 1, 2, 3, 4;
     std::cout << vector << std::endl;
 }
