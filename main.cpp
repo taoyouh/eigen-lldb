a9a153f1014d61dec678a0664a0c75384b117adf
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2021-04-23 02:51:38 +0000

    Update Testing Environment

--- main.cpp
+++ main.cpp
@@ -2,27 +2,10 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-struct my_storage {
-    int* data;
-    std::size_t count;
-};
-
-struct my_struct {
-    my_storage storage;
-};
-
-
 int main() {
-    my_struct obj;
-    obj.storage.data = new int[10];
-    obj.storage.count = 10;
-
-    std::cout << obj.storage.data[0] << std::endl;
-
-    Eigen::VectorXd vector(25, 1);
-    Eigen::MatrixXd matrix(25, 25);
-    vector[0] = 1;
-    vector[1] = 2;
-    vector[2] = 3;
+    Eigen::VectorXd vector(5, 1);
+    Eigen::MatrixXd matrix(5, 5);
+    Eigen::Vector3d vector3;
+    Eigen::Matrix3d matrix3;
     std::cout << vector << std::endl;
 }
